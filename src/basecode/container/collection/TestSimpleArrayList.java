0fbf9e80156d7fda28307201ce87f800ace44624
WanderNeng <zhangxuanneng@qq.com>
2018-09-02 10:47:02 +0800

    SimpleArrayList and Test

--- src/basecode/container/collection/TestSimpleArrayList.java
+++ src/basecode/container/collection/TestSimpleArrayList.java
@@ -1,20 +1,23 @@
 package basecode.container.collection;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class TestSimpleArrayList {
     
     public static void main(String[] args){
         SimpleArrayList<Integer> sal = new SimpleArrayList<Integer>();
         sal.add(5);
         sal.add(3);
-        sal.add(5);
+        sal.add(4);
         sal.add(52);
         sal.add(51);
-        sal.add(52);
+        sal.add(53);
         sal.add(56);
         sal.add(58);
         sal.add(2);
         sal.add(34);
-        sal.add(56);
+        sal.add(46);
         sal.add(95);
         sal.add(83);
         sal.add(24);
@@ -23,6 +26,18 @@ public class TestSimpleArrayList {
         sal.add(-9);
         sal.add(98);
         sal.add(21);
-        System.out.println(sal.getSize());
+        System.out.println(sal.get(1));//arraylist的index也是从0开始的
+        Iterator<Integer> it = sal.iterator();
+        while(it.hasNext()){
+            System.out.print(it.next()+" ");
+        }
+        SimpleArrayList<Integer> sal1 = (SimpleArrayList<Integer>) sal.clone();
+        Iterator<Integer> it1 = sal1.iterator();
+        System.out.println("");
+        while(it1.hasNext()){
+            System.out.print(it1.next()+" ");
+        }
+        System.out.println(sal1.getSize());
+
     }
 }
